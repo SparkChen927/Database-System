764d66c5ae329ccdea93d96c35517f46412c0749
SparkChen927 <516728164@qq.com>
2023-05-18 16:03:02 +0800

    优化sql文件，完成主界面的基本构造

--- purchase.sql
+++ purchase.sql
@@ -3,9 +3,11 @@ use purchase;
 create table suppliers(
     sno int primary key unique comment '供应商号',
     sname varchar(20) comment '供应商名',
+    type varchar(8) comment '类别',
     address varchar(50) comment '联系地址',
     settlement varchar(4) comment '结算方式',
     credit varchar(1) comment '信用等级',
+    check ( type in ('优先', '考察', '消极淘汰', '积极淘汰') ),
     check ( settlement in ('现金', '转账') ),
     check ( credit in ('A', 'B', 'C') )
 ) comment '供应商';
@@ -23,18 +25,18 @@ create table applications(
     gno int comment '货物编号',
     quantity int comment '购货数量',
     pname varchar(10) comment '采购员',
-    adate date comment '请购日期',
-    state varchar(6) comment '状态',
+    adate datetime default now() comment '请购日期',
+    astate varchar(6) comment '请购状态',
     foreign key (gno) references goods (gno),
-    check ( state in ('审核中', '已通过', '已拒绝') )
+    check ( astate in ('审核中', '已通过', '已拒绝') )
 ) comment '请购单';
 
 create table buying(
     bno int primary key unique comment '采购单号',
-    type varchar(4) comment '采购类型',
+    bstate varchar(4) comment '采购状态',
     bname varchar(10) comment '接货员',
-    bdate date comment '采购日期',
-    check ( type in ('收货', '退货') )
+    bdate datetime comment '采购日期',
+    check ( bstate in ('收货', '验货', '退货') )
 ) comment '采购单';
 
 create table documents(
